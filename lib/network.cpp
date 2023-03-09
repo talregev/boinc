47638b6cdfa071d3a5a768d3fc9b32325eb73d33
Tal Regev <tal.regev@gmail.com>
2023-03-09 17:10:47 +0200

    test websocket libs

--- lib/network.cpp
+++ lib/network.cpp
@@ -15,6 +15,9 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with BOINC.  If not, see <http://www.gnu.org/licenses/>.
 
+#include <ixwebsocket/IXWebSocketServer.h>
+#include <ixwebsocket/IXWebSocket.h>
+#include <iostream>
 #if defined(_WIN32)
 #include "boinc_win.h"
 #include <fcntl.h>
@@ -43,6 +46,103 @@
 
 using std::perror;
 using std::sprintf;
+
+void server() {
+    int port = 8008;
+    std::string host("127.0.0.1"); // If you need this server to be accessible on a different machine, use "0.0.0.0"
+    ix::WebSocketServer server(port, host);
+
+    server.setOnClientMessageCallback([](std::shared_ptr<ix::ConnectionState> connectionState, ix::WebSocket & webSocket, const ix::WebSocketMessagePtr & msg) {
+        // The ConnectionState object contains information about the connection,
+        // at this point only the client ip address and the port.
+        std::cout << "Remote ip: " << connectionState->getRemoteIp() << std::endl;
+
+        if (msg->type == ix::WebSocketMessageType::Open)
+        {
+            std::cout << "New connection" << std::endl;
+
+            // A connection state object is available, and has a default id
+            // You can subclass ConnectionState and pass an alternate factory
+            // to override it. It is useful if you want to store custom
+            // attributes per connection (authenticated bool flag, attributes, etc...)
+            std::cout << "id: " << connectionState->getId() << std::endl;
+
+            // The uri the client did connect to.
+            std::cout << "Uri: " << msg->openInfo.uri << std::endl;
+
+            std::cout << "Headers:" << std::endl;
+            for (auto it : msg->openInfo.headers)
+            {
+                std::cout << "\t" << it.first << ": " << it.second << std::endl;
+            }
+        }
+        else if (msg->type == ix::WebSocketMessageType::Message)
+        {
+            // For an echo server, we just send back to the client whatever was received by the server
+            // All connected clients are available in an std::set. See the broadcast cpp example.
+            // Second parameter tells whether we are sending the message in binary or text mode.
+            // Here we send it in the same mode as it was received.
+            std::cout << "Received: " << msg->str << std::endl;
+
+            webSocket.send(msg->str, msg->binary);
+        }
+    });
+
+    auto res = server.listen();
+    if (!res.first)
+    {
+        // Error handling
+    }
+
+    // Per message deflate connection is enabled by default. It can be disabled
+    // which might be helpful when running on low power devices such as a Rasbery Pi
+    server.disablePerMessageDeflate();
+
+    // Run the server in the background. Server can be stoped by calling server.stop()
+    server.start();
+
+    // Block until server.stop() is called.
+    server.wait();    
+}
+
+void client() {
+    // Our websocket object
+    ix::WebSocket webSocket;
+
+    std::string url("ws://localhost:8080/");
+    webSocket.setUrl(url);
+
+    // Optional heart beat, sent every 45 seconds when there is not any traffic
+    // to make sure that load balancers do not kill an idle connection.
+    webSocket.setPingInterval(45);
+
+    // Per message deflate connection is enabled by default. You can tweak its parameters or disable it
+    webSocket.disablePerMessageDeflate();
+
+    // Setup a callback to be fired when a message or an event (open, close, error) is received
+    webSocket.setOnMessageCallback([](const ix::WebSocketMessagePtr& msg)
+        {
+            if (msg->type == ix::WebSocketMessageType::Message)
+            {
+                std::cout << msg->str << std::endl;
+            }
+        }
+    );
+
+    // Now that our callback is setup, we can start our background thread and receive messages
+    webSocket.start();
+
+    // Send a message to the server (default to TEXT mode)
+    webSocket.send("hello world");
+
+    // The message can be sent in BINARY mode (useful if you send MsgPack data for example)
+    webSocket.sendBinary("some serialized binary data");
+
+    // ... finally ...
+
+    // Stop the connection
+    webSocket.stop();
+}
 
 const char* socket_error_str() {
     static char buf[80];
