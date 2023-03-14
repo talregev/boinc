f798be8fcab2a64e81838a35e27e13fcda1ad44f
Tal Regev <tal.regev@gmail.com>
2023-03-14 15:54:54 +0200

    Compile for mingw

--- 3rdParty/vcpkg_ports/vcpkg_link.sh
+++ 3rdParty/vcpkg_ports/vcpkg_link.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-export VCPKG_LINK="https://github.com/dg0yt/vcpkg -b openssl"
+export VCPKG_LINK="https://github.com/talregev/vcpkg -b TalR/ixwebsocket_mingw"
 echo VCPKG_LINK: $VCPKG_LINK
